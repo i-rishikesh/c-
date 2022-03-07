983bcf6d19362ae4108ee0811d6445e28a9a2fa4
Rishikesh <rishikeshmishra2102@gmail.com>
2022-03-08 00:53:14 +0530

    if else and loop

---
+++ maxnos.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cout<<"enter 2 nos:";
+    cin>>x>>y;
+    if(x>y)
+      cout<<"Max is"<<x;
+    else
+    cout<<"max is"<<y;
+    return 0;
+}
---
+++ typedef.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+typedef int marks;
+typedef int rollnu;
+int main()
+{
+    marks m1,m2;
+    rollnu r1,r2;
+    m1=70;
+    m2=80;
+    r1=2;
+    r2=3;
+    cout<<m1<< " "<<r1<<endl;
+
+}
